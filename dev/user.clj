98941fa2303292ce7210d3af65c7e75017322c47
Simon Katz <sk@nomistech.com>
2014-03-21 14:21:31 +0000

    Move dev stuff from projects to lein personal profile.

--- dev/user.clj
+++ dev/user.clj
@@ -2,30 +2,6 @@
   "Namespace to support hacking at the REPL.")
 
 ;;;; ___________________________________________________________________________
-;;;; Dev utils -- can be easily grabbed from any namespace.
-
-(defn setup-dev-stuff []
-  (do ; Require dev tools
-    (apply require [;; ---- I know these (ish)
-                    '[clojure.tools.namespace.repl :refer :all]
-                    '[clojure.tools.namespace.move :refer :all]
-                    '[clojure.repl :refer :all]
-                    '[clojure.pprint :refer :all]
-                    '[spyscope.core]
-                    '[cemerick.pomegranate :refer [add-dependencies]]
-                    '[com.nomistech.clojure-dev-utils :refer :all]
-                    ;; ---- To learn
-                    '[clojure.java.io :as cjio]
-                    '[clojure.string :as str]
-                    '[clojure.java.classpath :as cjc]
-                    '[criterium.core :as crit]]))
-  (do ; Control printing
-    (alter-var-root #'*print-level* (constantly 30))
-    (alter-var-root #'*print-length* (constantly 1000))))
-
-(setup-dev-stuff)
-
-;;;; ___________________________________________________________________________
 ;;;; Clojure workflow.
 ;;;; See:
 ;;;; - http://nomistech.blogspot.co.uk/2013/06/stuart-sierras-clojure-development_18.html
